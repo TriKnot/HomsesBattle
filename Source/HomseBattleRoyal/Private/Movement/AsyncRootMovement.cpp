8d5ee1850a15919e719c87309cc1a9ba0c62c1db
TriKnot <90341216+TriKnot@users.noreply.github.com>
2025-02-10 10:57:33 +0100

    Expose ApplyRootMotionSource to AS

---
+++ Source/HomseBattleRoyal/Private/Movement/AsyncRootMovement.cpp
@@ -0,0 +1,90 @@
+﻿#include "Movement/AsyncRootMovement.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+UAsyncRootMovement* UAsyncRootMovement::ApplyConstantForce(UCharacterMovementComponent* MovementComponent, FVector WorldDirection, float Strength, float Duration, bool bIsAdditive,
+	UCurveFloat* StrengthOverTime, bool bEnableGravity, ERootMotionFinishVelocityMode FinishVelocityMode, FVector SetVelocityOnFinish, float ClampVelocityOnFinish)
+{
+    UAsyncRootMovement* RootMovement = NewObject<UAsyncRootMovement>();
+	if (!MovementComponent)
+	{
+		RootMovement->MovementState = ERootMotionState::Failed;
+		RootMovement->OnMovementFailed.Broadcast();
+		RootMovement->Cancel();
+	}
+
+	RootMovement->CharacterMovementComponent = MovementComponent;
+    RootMovement->ContextWorld = GEngine->GetWorldFromContextObject(MovementComponent, EGetWorldErrorMode::ReturnNull);
+	
+    if (!ensureAlwaysMsgf(IsValid(MovementComponent), TEXT("World Context was not valid.")))
+    {
+    	RootMovement->MovementState = ERootMotionState::Failed;
+    	RootMovement->OnMovementFailed.Broadcast();
+    	RootMovement->Cancel();
+        return RootMovement;
+    }
+
+	TSharedPtr<FRootMotionSource_ConstantForce> ConstantForce = MakeShared<FRootMotionSource_ConstantForce>();
+	ConstantForce->Priority = 5; // TODO: Evaluate how to best set this
+	ConstantForce->Force = WorldDirection * Strength;
+	ConstantForce->Duration = Duration;
+	
+	ConstantForce->AccumulateMode = bIsAdditive ? ERootMotionAccumulateMode::Additive : ERootMotionAccumulateMode::Override;
+	ConstantForce->StrengthOverTime = StrengthOverTime;
+	ConstantForce->FinishVelocityParams.Mode = FinishVelocityMode;
+	ConstantForce->FinishVelocityParams.SetVelocity = SetVelocityOnFinish;
+	ConstantForce->FinishVelocityParams.ClampVelocity = ClampVelocityOnFinish;
+	
+	if (bEnableGravity)
+	{
+		ConstantForce->Settings.SetFlag(ERootMotionSourceSettingsFlags::IgnoreZAccumulate);
+	}
+	
+	RootMovement->RootMotionSourceID = MovementComponent->ApplyRootMotionSource(ConstantForce);
+	RootMovement->RegisterWithGameInstance(RootMovement->ContextWorld->GetGameInstance());
+	RootMovement->MovementState = ERootMotionState::Ongoing;
+
+	FTimerManager& TimerManager = RootMovement->ContextWorld->GetTimerManager();
+
+	TimerManager.SetTimer(
+		RootMovement->OngoingDelay,
+		FTimerDelegate::CreateLambda([WeakThis = TWeakObjectPtr<UAsyncRootMovement>(RootMovement)]()
+	{
+		if (WeakThis.IsValid() && WeakThis->IsActive())
+		{
+			WeakThis->MovementState = ERootMotionState::Finished;
+			WeakThis->OnMovementFinished.Broadcast();
+			WeakThis->Cancel();
+		}
+	}),
+		Duration,
+		false
+		);
+	return RootMovement;
+}
+
+void UAsyncRootMovement::Cancel()
+{
+    Super::Cancel();
+ 
+    if (!OngoingDelay.IsValid())
+    {
+    	return;
+    }
+	
+	const UWorld* World = GetWorld();
+    if (!World)
+    {
+        return;
+    }
+	
+    CharacterMovementComponent->RemoveRootMotionSourceByID(RootMotionSourceID);
+
+    FTimerManager& TimerManager = World->GetTimerManager();
+    TimerManager.ClearTimer(OngoingDelay);
+
+    if (MovementState == ERootMotionState::Ongoing)
+	{
+		MovementState = ERootMotionState::Cancelled;
+	}
+}
---
+++ Source/HomseBattleRoyal/Public/Movement/AsyncRootMovement.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/CancellableAsyncAction.h"
+#include "GameFramework/RootMotionSource.h"
+#include "AsyncRootMovement.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FMovementEvent);
+
+UENUM(BlueprintType)
+enum class ERootMotionState : uint8
+{
+	Idle        UMETA(DisplayName = "Idle"),
+	Ongoing     UMETA(DisplayName = "Ongoing"),
+	Finished    UMETA(DisplayName = "Finished"),
+	Failed      UMETA(DisplayName = "Failed"),
+	Cancelled   UMETA(DisplayName = "Cancelled"),
+};
+
+UCLASS(BlueprintType)
+class HOMSEBATTLEROYAL_API UAsyncRootMovement : public UCancellableAsyncAction
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TWeakObjectPtr<UWorld> ContextWorld = nullptr;
+	FTimerHandle OngoingDelay;
+
+	UPROPERTY()
+	UCharacterMovementComponent* CharacterMovementComponent;
+	
+	bool bEnableGravity;
+	
+	uint16 RootMotionSourceID;
+
+	UPROPERTY(BlueprintAssignable)
+	FMovementEvent OnMovementFinished;
+
+	UPROPERTY(BlueprintAssignable)
+	FMovementEvent OnMovementFailed;
+
+public:
+
+	UPROPERTY(BlueprintReadOnly)
+	ERootMotionState MovementState = ERootMotionState::Idle;
+
+	UFUNCTION(ScriptCallable, Category = "RootMovement")
+	static UAsyncRootMovement* ApplyConstantForce(
+		UCharacterMovementComponent* MovementComponent,
+		FVector WorldDirection,
+		float Strength,
+		float Duration,  
+		bool bIsAdditive = false,  
+		UCurveFloat* StrengthOverTime = nullptr,  
+		bool bEnableGravity = false,  
+		ERootMotionFinishVelocityMode FinishVelocityMode = ERootMotionFinishVelocityMode::MaintainLastRootMotionVelocity,  
+		FVector SetVelocityOnFinish = FVector::ZeroVector,  
+		float ClampVelocityOnFinish = 0.0f  
+	);
+	
+	virtual void Cancel() override;
+
+	virtual UWorld* GetWorld() const override
+	{
+		return ContextWorld.IsValid() ? ContextWorld.Get() : nullptr;
+	}
+	
+};
