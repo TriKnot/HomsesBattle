4cc920368d1dac90ec657d3b08be97401ed24da7
TriKnot <90341216+TriKnot@users.noreply.github.com>
2025-03-10 11:04:16 +0100

    Expose Duplicate UObject to BP and AS

---
+++ Source/HomseBattleRoyal/Private/Libraries/DuplicateObjectBlueprintLibrary.cpp
@@ -0,0 +1,12 @@
+﻿#include "Libraries/DuplicateObjectBlueprintLibrary.h"
+
+UObject* UDuplicateObjectBlueprintLibrary::DuplicateObjectBlueprint(UObject* SourceObject, UObject* Outer)
+{
+	// Basic validation: if either parameter is null, return null.
+	if (!SourceObject || !Outer)
+	{
+		return nullptr;
+	}
+	// Duplicate the object using Unreal's built-in DuplicateObject.
+	return DuplicateObject(SourceObject, Outer);
+}
---
+++ Source/HomseBattleRoyal/Public/Libraries/DuplicateObjectBlueprintLibrary.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DuplicateObjectBlueprintLibrary.generated.h"
+
+
+UCLASS()
+class HOMSEBATTLEROYAL_API UDuplicateObjectBlueprintLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "Utilities")
+	static UObject* DuplicateObjectBlueprint(UObject* SourceObject, UObject* Outer);
+};
